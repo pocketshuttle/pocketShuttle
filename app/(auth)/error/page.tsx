47f312629d4f5723508cb1033717d819f4d30068
Abusantos <abusomwansantos@gmail.com>
2024-07-04 00:48:30 +0100

    created a login button

---
+++ app/(auth)/error/page.tsx
@@ -0,0 +1,9 @@
+import AuthErrorCard from "@/components/auth/auth-error"
+
+const AuthError = () => {
+    return (
+        // < AuthErrorCard />
+    )
+}
+
+export default AuthError
---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,36 @@
+// "use client"
+
+// import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
+// import { AuthHeader } from "@/components/auth/header"
+// import { Social } from "@/components/auth/social"
+// import { BackButton } from "@/components/auth/back-button"
+
+// interface CardWrapperProps {
+//     children: React.ReactNode
+//     headLabel: string
+//     backButtonLabel: string
+//     backButtonHref: string
+//     showSocial?: boolean
+// }
+// export const CardWrapper = ({ children, headLabel, backButtonHref, backButtonLabel, showSocial }: CardWrapperProps) => {
+//     return (
+//         <Card className="w-[400px] shadow-md">
+//             <CardHeader>
+//                 <AuthHeader label={headLabel} />
+//             </CardHeader>
+//             <CardContent>
+//                 {children}
+//             </CardContent>
+//             {
+//                 showSocial && (
+//                     <CardFooter>
+//                         <Social />
+//                     </CardFooter>
+//                 )
+//             }
+//             <CardFooter>
+//                 <BackButton href={backButtonHref} label={backButtonLabel} />
+//             </CardFooter>
+//         </Card>
+//     )
+// }
