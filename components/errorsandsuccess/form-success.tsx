807e30f80f7a67ecba2cc61ba4e2365ffb1f7c7a
Abusantos <abusomwansantos@gmail.com>
2024-07-04 01:57:34 +0100

    added a form sucess and error

---
+++ components/errorsandsuccess/form-success.tsx
@@ -0,0 +1,21 @@
+// import { CheckCircleIcon } from "@heroicons/react/20/solid";
+
+import { CheckCircledIcon } from "@radix-ui/react-icons";
+
+interface FormSuccessProps {
+    message?: string;
+}
+
+export const FormSuccess = ({ message }: FormSuccessProps) => {
+    if (!message) {
+        return null
+    }
+
+    return (
+        <div className="bg-emerald-500/15 rounded-md p-3 flex items-center gap-x-2 text-sm text-emerald-500">
+            <CheckCircledIcon className="w-4 h-4" />
+            <p>{message}</p>
+        </div>
+    )
+
+}
