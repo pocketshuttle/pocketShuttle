67a7c2335eec5a9ade4199279ffcea00b98312d8
Abusantos <abusomwansantos@gmail.com>
2024-07-04 03:27:11 +0100

    added a prisma db

--- lib/db.ts
+++ lib/db.ts
@@ -1,45 +1,12 @@
-// // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
+import { PrismaClient } from "@prisma/client";
 
-// import { PrismaClient,  } from "@prisma/client";
+// type for prisma
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-// // import { PrismaClient, ServerApiVersion } from "prisma";
-// if (!process.env.DATABASE_URL) {
-//   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
-// }
+//preventing hextjs hotreload, preventing reinitialization of prisma client
+//so we stored the connection in the global this, so on reload we check if already have this as global isnt affected by hotreload
+export const db = globalThis.prisma || new PrismaClient();
 
-// const uri = process.env.MONGODB_URI;
-// if (!uri) {
-//   console.log("Mongo Uri isnt defined");
-// }
-// const options = {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// };
-
-// let client;
-// let clientPromise: Promise<PrismaClient>;
-
-// if (process.env.NODE_ENV === "development") {
-//   // In development mode, use a global variable so that the value
-//   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-//   let globalWithMongo = global as typeof globalThis & {
-//     _mongoClientPromise?: Promise<MongoClient>;
-//   };
-
-//   if (!globalWithMongo._mongoClientPromise) {
-//     client = new MongoClient(uri, options);
-//     globalWithMongo._mongoClientPromise = client.connect();
-//   }
-//   clientPromise = globalWithMongo._mongoClientPromise;
-// } else {
-//   // In production mode, it's best to not use a global variable.
-//   client = new MongoClient(uri, options);
-//   clientPromise = client.connect();
-// }
-
-// // Export a module-scoped MongoClient promise. By doing this in a
-// // separate module, the client can be shared across functions.
-// export default clientPromise;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
