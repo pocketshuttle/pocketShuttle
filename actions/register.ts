9db6bd70b982704f3027d9634fc016de659e6cbf
Abusantos <abusomwansantos@gmail.com>
2024-07-04 04:11:32 +0100

    successfully aded to neon

--- actions/register.ts
+++ actions/register.ts
@@ -2,14 +2,39 @@
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
 import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
+
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
-  if (!validatedFields) {
+  if (!validatedFields.success) {
     return { error: "Invalid Fields" };
   }
 
-  // newKid.password = hashPassword;
+  const { schoolname, email, password } = validatedFields.data;
 
-  return { success: "Succesfully Registered" };
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const existingUser = await db.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return { error: "Email already in use!" };
+    }
+
+    await db.user.create({
+      data: {
+        name: schoolname,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return { success: "Successfully Registered" };
+  } catch (error) {
+    console.error("Error during registration:", error);
+    return { error: "Registration failed. Please try again." };
+  }
 };
