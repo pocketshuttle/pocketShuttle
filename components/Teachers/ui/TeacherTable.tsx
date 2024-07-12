e2af7b1398f73f22e44475c4e38a5bafb84f630d
Abusantos <abusomwansantos@gmail.com>
2024-07-12 13:43:55 +0100

    added a shadcn table

---
+++ components/Teachers/ui/TeacherTable.tsx
@@ -0,0 +1,102 @@
+"use client"
+import { Search } from "@/components/dashboard/search/search"
+import { Button } from "@/components/ui/button"
+import dashboard from "@/public/images/dashboard.svg"
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+import { DriverAndTeacherModal } from "./teachers-modal"
+import { Pagination } from "@/components/dashboard/pagination/pagination"
+import Link from "next/link"
+import { useFetch } from "@/hooks/useFetch"
+import { useSession } from "next-auth/react"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+
+
+export const TeachersTable = () => {
+    const { data: session } = useSession()
+    const userId = session?.user?.id
+    const { data: teachersData, isPending, errorMessage } = useFetch(`/api/addteacher/${userId}`, userId);
+    if (isPending) {
+        return <p>Loading...</p>;
+    }
+
+    console.log(teachersData)
+
+    if (errorMessage) {
+        return <p>Error: {errorMessage}</p>;
+    }
+    return (
+        <Table>
+            <TableHeader>
+                <TableRow className="text-gray-300">
+                    <TableHead className="w-[250px]">Full Name</TableHead>
+                    <TableHead>Email</TableHead>
+                    <TableHead>Phone Number</TableHead>
+                    <TableHead className="">Address</TableHead>
+                    <TableHead className="">Bus</TableHead>
+                </TableRow>
+
+            </TableHeader>
+
+            {/* <TableCaption>Teachers Table</TableCaption> */}
+            <TableBody className="text-[0.8rem] text-[var(--textSoft)]">
+                {
+                    // teachersData && teachersData > 0 ? (
+                    teachersData?.map((teacher) => {
+                        console.log(teacher)
+                        return (
+                            <TableRow key={teacher.id}>
+                                <TableCell className="">
+                                    <div className="flex items-center gap-2">
+                                        {/* <Image src={dashboard} alt="student name" className="rounded-full object-cover" /> */}
+                                        <span className="">{teacher.full_name}</span>
+                                    </div>
+                                </TableCell>
+                                <TableCell>
+                                    {teacher.email}
+                                </TableCell>
+                                <TableCell>
+                                    {teacher.phoneNumber}
+                                </TableCell>
+                                <TableCell>
+                                    {teacher.address}
+                                </TableCell>
+                                <TableCell className="text-[0.7rem]">
+                                    {teacher.busId || "No Bus"}
+                                </TableCell>
+                                <TableCell>
+                                    <div className="space-x-2 flex">
+                                        <Link href={`/dashboard/teachers/${teacher._id}`}>
+                                            <button className="bg-[teal] px-2 text-[0.5rem] rounded-sm">
+                                                view
+                                            </button>
+                                        </Link>
+                                        <Link href="/dashboard">
+                                            <button className="bg-destructive px-2 text-[0.5rem] rounded-sm">
+                                                delete
+                                            </button>
+                                        </Link>
+                                    </div>
+                                </TableCell>
+
+                            </TableRow>
+                        )
+
+                    })
+                    // ) : ""
+                }
+
+            </TableBody>
+        </Table>
+    )
+}
