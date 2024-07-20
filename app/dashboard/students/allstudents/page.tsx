4300d6b9852d5c051aa3f65e0b9f1cf821300a49
Abusantos <abusomwansantos@gmail.com>
2024-07-20 23:15:23 +0100

    fetching all students

--- app/dashboard/students/allstudents/page.tsx
+++ app/dashboard/students/allstudents/page.tsx
@@ -1,13 +1,27 @@
 "use client"
+import StudentCard from "@/components/students/ui/student-card";
 import { useFetch } from "@/hooks/useFetch";
 import { useSession } from "next-auth/react"
 
 const AllStudents = () => {
-    const { data: studentsData, isPending, errorMessage } = useFetch(`/api/addstudent/${userId}`, userId);
     const { data: session } = useSession()
     const userId = session?.user?.id
+
+    const { data: studentsData, isPending, errorMessage } = useFetch(`/api/addstudent/${userId}`, userId);
+    console.log(studentsData)
     return (
-        <div>AllStudents</div>
+        <div className="mt-4">
+            {isPending && <p>Loading...</p>}
+            {errorMessage && <p>{errorMessage}</p>}
+            <div className=" flex gap-4">
+
+
+                {studentsData && studentsData.map((student) => (
+                    <StudentCard data={student} />
+                ))}
+
+            </div>
+        </div>
     )
 }
 
