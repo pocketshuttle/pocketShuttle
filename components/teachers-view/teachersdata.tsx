c014f80c5e2a273d35c3bf867c5da20070e11c63
Abusantos <abusomwansantos@gmail.com>
2024-07-25 10:07:32 +0100

    added a populate to student

--- components/teachers-view/teachersdata.tsx
+++ components/teachers-view/teachersdata.tsx
@@ -43,79 +43,98 @@ export const TeachersViewData = () => {
 
     const [isOpenModal, setIsOpenModal] = useState<boolean>(false)
     const { data: studentsData, isPending, errorMessage } = useFetch(`/api/addstudent/${userId}`, userId);
+    const { data: busData, isPending: busLoading, } = useFetch(`/api/addteacher/${userId}`, userId);
 
     const [attendance, SetAttendance] = useState("")
 
-    console.log(studentsData)
+    console.log(busData)
     return (
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg bg-[#182237]">
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg bg-[var(--bgSoft)]">
 
             {/* {
                 isOpenModal && <StudentModal isOpenModal={isOpenModal} setIsOpenModal={setIsOpenModal} />
             } */}
-
-            <Table>
-                <TableHeader>
-                    <TableRow className=" uppercase text-[0.7rem]">
-                        <TableHead className="w-[250px]">Full Name</TableHead>
-                        <TableHead>Gender</TableHead>
-                        <TableHead className="">Grade</TableHead>
-                        <TableHead className="w-[250px]">Address</TableHead>
-                        <TableHead className="">Attendance</TableHead>
-                        <TableHead>Status</TableHead>
-                    </TableRow>
-
-                </TableHeader>
-                <TableBody className="text-[0.8rem] text-[var(--textSoft)]">
-                    {
-                        studentsData && studentsData?.map((student: StudentProps) => {
-                            return (
-                                <TableRow key={student._id}>
-                                    <TableCell className="">
-                                        <div className="flex items-center gap-2">
-                                            <Image src={student.image && student.image || dashboard} alt={student.full_name} className="rounded-md object-cover w-9 h-9" width={100} height={100} />
-                                            <span className="">{student.full_name}</span>
-                                        </div>
-                                    </TableCell>
-                                    <TableCell>
-                                        {student.gender}
-                                    </TableCell>
-
-                                    <TableCell>
-                                        {student.grade}
-                                    </TableCell>
-                                    <TableCell>
-                                        {student.address}
-                                    </TableCell>
-                                    <TableCell>
-                                        <  AttendaceTab label1="Present" label2="Absent" data={student.attendance} value1="present" value2="absent" SetAttendance={SetAttendance} attendance={attendance} id={student._id} />
-                                    </TableCell>
-                                    <TableCell>
-                                        {
-                                            student.attendance === "present" ?
-                                                <  AttendaceTab label1="Dropped" label2="Picked" data={student.status} value1="dropped" value2="picked" SetAttendance={SetAttendance} attendance={attendance} id={student._id} /> : ""
-                                        }
-                                    </TableCell>
-
-                                    <TableCell>
-                                        <div className="space-x-2">
-                                            <Link href={`/dashboard/students/${student._id}`}>
-                                                <button className="bg-[teal] px-2 text-[0.5rem] rounded-sm">
-                                                    view
-                                                </button>
-                                            </Link>
-                                        </div>
-                                    </TableCell>
-                                </TableRow>
-                            )
-                        })
-                    }
-                </TableBody>
-            </Table>
+            {
 
 
-            <Pagination />
-        </div >
+            }
+
+            <div className="mt-4 bg-[var(--bg)] lg:hidden">
+                <header className=" px-4 py-1 space-y-2 text-lg">
+                    <p>
+                        BusName : <span>{ }</span>
+                    </p>
+                    <p>
+                        Driver : <span></span>
+                    </p>
+                </header>
+            </div>
+
+
+            <div className="hidden lg:flex lg:flex-col">
+                <Table>
+                    <TableHeader>
+                        <TableRow className=" uppercase text-[0.7rem]">
+                            <TableHead className="w-[250px]">Full Name</TableHead>
+                            <TableHead>Gender</TableHead>
+                            <TableHead className="">Grade</TableHead>
+                            <TableHead className="w-[250px]">Address</TableHead>
+                            <TableHead className="">Attendance</TableHead>
+                            <TableHead>Status</TableHead>
+                        </TableRow>
+
+                    </TableHeader>
+                    <TableBody className="text-[0.8rem] text-[var(--textSoft)]">
+                        {
+                            studentsData && studentsData?.map((student: StudentProps) => {
+                                return (
+                                    <TableRow key={student._id}>
+                                        <TableCell className="">
+                                            <div className="flex items-center gap-2">
+                                                <Image src={student.image && student.image || dashboard} alt={student.full_name} className="rounded-md object-cover w-9 h-9" width={100} height={100} />
+                                                <span className="">{student.full_name}</span>
+                                            </div>
+                                        </TableCell>
+                                        <TableCell>
+                                            {student.gender}
+                                        </TableCell>
+
+                                        <TableCell>
+                                            {student.grade}
+                                        </TableCell>
+                                        <TableCell>
+                                            {student.address}
+                                        </TableCell>
+                                        <TableCell>
+                                            <  AttendaceTab label1="Present" label2="Absent" data={student.attendance} value1="present" value2="absent" SetAttendance={SetAttendance} attendance={attendance} id={student._id} />
+                                        </TableCell>
+                                        <TableCell>
+                                            {
+                                                student.attendance === "present" ?
+                                                    <  AttendaceTab label1="Dropped" label2="Picked" data={student.status} value1="dropped" value2="picked" SetAttendance={SetAttendance} attendance={attendance} id={student._id} /> : ""
+                                            }
+                                        </TableCell>
+
+                                        <TableCell>
+                                            <div className="space-x-2">
+                                                <Link href={`/dashboard/students/${student._id}`}>
+                                                    <button className="bg-[teal] px-2 text-[0.5rem] rounded-sm">
+                                                        view
+                                                    </button>
+                                                </Link>
+                                            </div>
+                                        </TableCell>
+                                    </TableRow>
+                                )
+                            })
+                        }
+                    </TableBody>
+                </Table>
+
+
+                <Pagination />
+            </div >
+        </div>
 
     )
 }
