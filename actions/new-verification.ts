c15b79686e34428321a9f31820ce28bcd569389f
Abusantos <abusomwansantos@gmail.com>
2024-08-01 04:20:16 +0100

    added a new verification server action

---
+++ actions/new-verification.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import User from "@/(models)/User";
+import VerificationToken from "@/(models)/VerificationToken";
+import { getUserByEmail } from "@/data/user";
+import { getVerificationTokenByToken } from "@/data/verification-token";
+import { connectToDB } from "@/utils/connect-to-db";
+
+export const newVerification = async (token: string) => {
+  await connectToDB();
+  const existingToken = await getVerificationTokenByToken(token);
+  if (!existingToken) {
+    return { error: "Token not found" };
+  }
+
+  const hasExpired = new Date(existingToken.expires) < new Date();
+  if (hasExpired) {
+    return { error: "Token expired" };
+  }
+
+  const existingUser = await getUserByEmail(existingToken.email);
+  if (!existingUser) {
+    return { error: "email doesnt exist" };
+  }
+
+  await User.findByIdAndUpdate(
+    existingUser._id,
+    { emailVerified: new Date(), email: existingToken.email },
+    { email: existingToken.email }
+  );
+
+  await VerificationToken.deleteOne({ _id: existingToken._id });
+
+  return { success: "email verified" };
+};
---
+++ app/(auth)/new-verification/page.tsx
@@ -0,0 +1,12 @@
+import NewVerificationForm from '@/components/auth/new-verification-form'
+import React from 'react'
+
+const NewVerification = () => {
+    return (
+        <div className='flex items-center justify-center m-auto'>
+            < NewVerificationForm />
+        </div>
+    )
+}
+
+export default NewVerification
