7279ece3ae893ab5f8f6298e6a107c801a63b184
Abusantos <abusomwansantos@gmail.com>
2024-08-01 02:33:25 +0100

    added a verification for unconfirmed email

---
+++ data/verification-token.ts
@@ -0,0 +1,22 @@
+import VerificationToken from "@/(models)/VerificationToken";
+import { connectToDB } from "@/utils/connect-to-db";
+export const getVerificationTokenByEmail = async (email: string) => {
+  try {
+    await connectToDB();
+    const verificationToken = VerificationToken.find({ email: email });
+    return verificationToken;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+export const getVerificationTokenByToken = async (token: string) => {
+  try {
+    await connectToDB();
+    const verificationToken = VerificationToken.findOne({ token: token });
+    return verificationToken;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
