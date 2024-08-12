d1f815f17178db9f46f1e403eecee5152a248e07
Abusantos <abusomwansantos@gmail.com>
2024-08-12 02:13:25 +0100

    centered the reg form

--- app/(auth)/new-verification/page.tsx
+++ app/(auth)/new-verification/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const NewVerification = () => {
     return (
-        <div className='flex items-center justify-center m-auto'>
+        <div className='flex items-center justify-center h-screen'>
             < NewVerificationForm />
         </div>
     )
--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 const ResetPassword = () => {
     return (
-        <NewPasswordForm />
+        <div className='flex items-center justify-center h-screen'>
+            <NewPasswordForm />
+        </div>
     )
 }
 
