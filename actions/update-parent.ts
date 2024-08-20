33667d3b1edbc5991fbe2ab56dd3a91439587959
Abusantos <abusomwansantos@gmail.com>
2024-08-20 01:41:10 +0100

    added update for parent

---
+++ actions/update-parent.ts
@@ -0,0 +1,32 @@
+"use server";
+import { db } from "@/lib/db";
+import bcrypt from "bcryptjs";
+
+export const updateParent = async (parentId: string | undefined, data: any) => {
+  try {
+    const updateData: any = {
+      schoolId: data.school_id,
+      busId: data.busId || undefined,
+      full_name: data.full_name,
+      address: data.address,
+      image: data.image,
+      email: data.email,
+      role: data.role,
+    };
+
+    if (data.password) {
+      const hashedPassword = await bcrypt.hash(data.password, 10);
+      updateData.password = hashedPassword;
+    }
+
+    await db.parent.update({
+      where: { id: parentId },
+      data: updateData,
+    });
+
+    return { message: "Parent updated successfully" };
+  } catch (error) {
+    console.error("Error updating parent:", error);
+    return { message: "Error updating parent" };
+  }
+};
