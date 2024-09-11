463781919fc4697a8073f9de2078e949ed8d38a8
Abusantos <abusomwansantos@gmail.com>
2024-09-11 22:43:24 +0100

    updated web metadata

--- routes.ts
+++ routes.ts
@@ -19,8 +19,6 @@ export const authRoutes = [
   "/reset",
   "/reset-password",
 ];
-// https://pocketshuttle.vercel.app/dashboard
-// http://localhost:3000/dashboard
 /**
  *  the prefix for api auth routes,
  * routes with this prefix are used for api auth
