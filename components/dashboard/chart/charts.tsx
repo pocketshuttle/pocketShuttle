8c213f27a67db21707240495164e2fe3f36d6819
Abusantos <abusomwansantos@gmail.com>
2024-09-11 22:12:53 +0100

    updated web manifest

--- components/dashboard/chart/charts.tsx
+++ components/dashboard/chart/charts.tsx
@@ -4,7 +4,7 @@ import { Poppins } from "next/font/google"
 const poppins = Poppins({ weight: "500", subsets: ["latin"] })
 export const Charts = () => {
     return (
-        <div className="bg-[var(--bgSoft)] p-4">
+        <div className="bg-[var( --bg-root)] p-4">
 
             <h1 className={`${poppins} text-[var(--textSoft)] py-4`}>Weekly Students Chart</h1>
             <div className="h-[400px] w-full">
