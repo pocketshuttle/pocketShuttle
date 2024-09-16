8f02610a79ec570856879d3ffc33af1ac7891189
Abusantos <abusomwansantos@gmail.com>
2024-09-16 16:08:39 +0100

    reconfigured

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,27 +1,48 @@
-import NextAuth, { type DefaultSession } from "next-auth";
-// import { JWT, DefaultJWT } from "next-auth/jwt";
-import { UserRole } from "@prisma/client";
+// import NextAuth, { type DefaultSession } from "next-auth";
+// // import { JWT, DefaultJWT } from "next-auth/jwt";
+// import { UserRole } from "@prisma/client";
 
-export type ExtendedUser = DefaultSession["user"] & {
-  role: UserRole;
-};
+// export type ExtendedUser = DefaultSession["user"] & {
+//   role: UserRole;
+// };
+// // declare module "next-auth" {
+// //   interface Session {
+// //     user: {
+// //       id: string;
+// //       role: string;
+// //       name: string;
+// //       image: string;
+// //     } & DefaultSession;
+// //   }
+
+// //   interface User extends DefaultUser {
+// //     role: string;
+// //   }
+// // }
+
 // declare module "next-auth" {
-//   interface Session {
-//     user: {
-//       id: string;
-//       role: string;
-//       name: string;
-//       image: string;
-//     } & DefaultSession;
-//   }
-
-//   interface User extends DefaultUser {
-//     role: string;
+//   interface session {
+//     user: ExtendedUser;
 //   }
 // }
+// types/next-auth.d.ts
+
+import "next-auth";
+import "next-auth/jwt";
 
 declare module "next-auth" {
-  interface session {
-    user: ExtendedUser;
+  interface User {
+    id: string;
+    role: string;
+  }
+  interface Session {
+    user: User;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    role: string;
   }
 }
