a0e9ca5ee86731338b367ea90eb4a0d054741cb0
Abusantos <abusomwansantos@gmail.com>
2024-09-19 00:20:02 +0100

    disabled cache

--- lib/session.ts
+++ lib/session.ts
@@ -10,7 +10,7 @@ import { redirect } from "next/navigation";
 //   return session?.user;
 // };
 
-export const getUserSession = cache(async () => {
+export const getUserSession = async () => {
   const cookie = cookies().get("session")?.value;
   console.log("Cookie value:", cookie);
 
@@ -27,7 +27,7 @@ export const getUserSession = cache(async () => {
   }
 
   return session;
-});
+};
 
 // export const verifySession = cache(async () => {
 //   const cookie = cookies().get("session")?.value;
