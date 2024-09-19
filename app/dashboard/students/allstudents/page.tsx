d115dd4e4a2ad4c2ce21599beecf021dffddf620
Abusantos <abusomwansantos@gmail.com>
2024-09-19 13:13:42 +0100

    added type check for student

--- app/dashboard/students/allstudents/page.tsx
+++ app/dashboard/students/allstudents/page.tsx
@@ -20,14 +20,19 @@ const AllStudents = async ({ searchParams }: { searchParams: { [key: string]: st
         ? decodeURIComponent(decodeURIComponent(searchParams.grade.replace(/\+/g, ' ')))
         : "";
     const ITEM_PER_PAGE = 10;
-    if (!user || user?.id !== "string") {
-        return <div className="flex items-center justify-center">
-            User session is not available. Please log in.
-            <LoginButton>
-                <Button size={"lg"} >Login</Button>
-            </LoginButton>
 
-        </div>
+    // if no user, that means you havent logged in, so redirect back to login page
+    if (!user || typeof user.id !== 'string') {
+        return (
+            <div className="flex items-center justify-center">
+                <div>
+                    User session is not available. Please log in.
+                    <LoginButton>
+                        <Button size={"lg"}>Login</Button>
+                    </LoginButton>
+                </div>
+            </div>
+        )
     }
 
     const userId = user?.id
