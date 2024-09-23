977cdb2c66081af6e1bc246e172114714a8f07cc
Abusantos <abusomwansantos@gmail.com>
2024-09-23 21:01:20 +0100

    working on help

--- app/dashboard/help/page.tsx
+++ app/dashboard/help/page.tsx
@@ -1,8 +1,13 @@
+import { FAQ } from '@/components/help/faq'
+import { HeroSection } from '@/components/help/hero'
 import React from 'react'
 
 const page = () => {
     return (
-        <div>Help</div>
+        <div >
+            <HeroSection />
+            <FAQ />
+        </div>
     )
 }
 
