984029a5bb099dd2d941608b9664c9e6bc199c16
Abusantos <abusomwansantos@gmail.com>
2024-10-06 13:04:47 +0100

    added OTW to the studentpresense schema

--- components/teachers-view/student-view/guardian.tsx
+++ components/teachers-view/student-view/guardian.tsx
@@ -17,6 +17,7 @@ type StudentProps = {
     parent: ParentProps;
     attendance: string
     status: string
+
 };
 
 type ParentProps = {
@@ -37,79 +38,76 @@ type ParentProps = {
  * @returns JSX.Element
  */
 
-export const GuardianPage = ({ data }: { data: { students: StudentProps[] } }) => {
+export const GuardianPage = ({ data }: { data: StudentProps }) => {
     const [attendance, SetAttendance] = React.useState("")
+
+    const siblings = data.parent.Student.filter((sibling_id) => sibling_id.id !== data.id)
 
     return (
         <main className="px-4 space-y-2 w-full">
             <h2>Guardian Profile</h2>
-            {data?.students?.map((student: StudentProps) => (
+            {data &&
                 <div
-                    key={student.id}
                     className="flex items-center bg-[var(--bgSoft)] py-2 px-4 space-x-4 rounded-md"
                 >
                     <Image
-                        src={student?.parent?.image || avatar}
-                        alt={student?.parent?.full_name}
+                        src={data?.parent?.image || avatar}
+                        alt={data?.parent?.full_name}
                         className="rounded-md object-cover w-24 h-24 shadow-lg"
                         width={100}
                         height={100}
                     />
                     <div className="py-4 space-y-2">
-                        <h2 className="capitalize">{student?.parent?.full_name}</h2>
+                        <h2 className="capitalize">{data?.parent?.full_name}</h2>
                         <p className="text-lg text-[var(--textSoft)]">
-                            {student.parent?.phoneNumber}
+                            {data.parent?.phoneNumber}
                         </p>
                         <p className="text-sm text-[var(--textSoft)]">
-                            {student.parent?.email}
+                            {data.parent?.email}
                         </p>
                         <p className="text-sm text-[var(--textSoft)]">
-                            {student.parent?.address}
+                            {data.parent?.address}
                         </p>
                     </div>
                 </div>
-            ))}
+            }
 
             <div>
                 <h3 className="text-center p-5">All kids</h3>
-                {data?.students?.map((student: StudentProps) => {
-                    const parentKids = student?.parent?.Student;
-
-                    return parentKids?.map((sibling: StudentProps) => {
-                        return (
-                            <div
-                                key={sibling.id}
-                                className="flex items-center bg-[var(--bgSoft)] py-2 px-4 space-x-4 rounded-md mb-2"
-                            >
-                                <Image
-                                    src={sibling?.image || avatar}
-                                    alt={sibling?.full_name}
-                                    className="rounded-md object-cover w-24 h-24 shadow-lg"
-                                    width={100}
-                                    height={100}
-                                />
-                                <div className="py-4 space-y-2">
-                                    <h2 className="capitalize text-gray-200">
-                                        {sibling?.full_name}
-                                    </h2>
+                {siblings.map((sibling: StudentProps) => {
+                    return (
+                        <div
+                            key={sibling.id}
+                            className="flex items-center bg-[var(--bgSoft)] py-2 px-4 space-x-4 rounded-md mb-2"
+                        >
+                            <Image
+                                src={sibling?.image || avatar}
+                                alt={sibling?.full_name}
+                                className="rounded-md object-cover w-24 h-24 shadow-lg"
+                                width={100}
+                                height={100}
+                            />
+                            <div className="py-4 space-y-2">
+                                <h2 className="capitalize text-gray-200">
+                                    {sibling?.full_name}
+                                </h2>
+                            </div>
+                            <div className="flex space-x-3 ">
+                                <div>
+                                    {
+                                        <  AttendanceTab label1="Present" label2="Absent" data={sibling.attendance} value1="PRESENT" value2="ABSENT" SetAttendance={SetAttendance} attendance={attendance} id={sibling.id} />
+                                    }
                                 </div>
-                                <div className="flex space-x-3 ">
-                                    <div>
-                                        {
-                                            <  AttendanceTab label1="Present" label2="Absent" data={sibling.attendance} value1="PRESENT" value2="ABSENT" SetAttendance={SetAttendance} attendance={attendance} id={sibling.id} />
-                                        }
-                                    </div>
-                                    <div>
-                                        {
-                                            sibling.attendance === "PRESENT" ?
-                                                <  AttendanceTab label1="Dropped" label2="Picked" data={sibling.status} value1="DROPPED" value2="PICKED"
-                                                    SetAttendance={SetAttendance} attendance={attendance} id={sibling.id} /> : ""
-                                        }
-                                    </div>
+                                <div>
+                                    {
+                                        sibling.attendance === "PRESENT" ?
+                                            <  AttendanceTab label1="Dropped" label2="Picked" data={sibling.status} value1="DROPPED" value2="PICKED"
+                                                SetAttendance={SetAttendance} attendance={attendance} id={sibling.id} /> : ""
+                                    }
                                 </div>
                             </div>
-                        );
-                    });
+                        </div>
+                    );
                 })}
             </div>
         </main>
