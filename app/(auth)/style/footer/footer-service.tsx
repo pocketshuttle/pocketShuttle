27ab1f39f0c4528d5e2767e497bb4797f660210e
Abusantos <abusomwansantos@gmail.com>
2024-10-19 23:00:09 +0100

    adding style to footer

---
+++ app/(auth)/style/footer/footer-service.tsx
@@ -0,0 +1,12 @@
+
+export const FooterService = () => {
+    return (
+        <div className="flex items-center justify-between px-2 space-x-2">
+            <small className="text-slate-500">Terms of Service</small>
+            <small className="text-slate-500">Cookies Policy</small>
+            <small className="text-slate-500">FAQ</small>
+            <small className="text-slate-500">Support</small>
+        </div>
+    )
+}
+
