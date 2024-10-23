92f164a41fabfbd00e2a250986c141685df33434
Abusantos <abusomwansantos@gmail.com>
2024-10-24 00:05:02 +0100

    added a new icon

--- app/(auth)/style/footer/footer.tsx
+++ app/(auth)/style/footer/footer.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React, { useRef } from 'react'
-import { FooterPlayer } from './footer-player'
 import Image from 'next/image'
 import { FooterService } from './footer-service'
 
@@ -8,11 +7,14 @@ export const AuthFooter = () => {
 
     return (
         <div className='flex flex-col  items-center px-3 py-5 space-y-4 '>
-           
+
             <div className='flex space-x-2 py-2 mt-3'>
-                <FooterPlayer playerLink='/images/Facebook.mp4' />
-                <FooterPlayer playerLink='/images/Tiktok.mp4' />
-                <FooterPlayer playerLink='/images/Twitter.mp4' />
+                <small className='h-10 w-10 rounded-full bg-slate-200 flex items-center justify-center'>
+                    <Image src="images/tiktok.svg" alt='tiktok' width={30} height={30} className='rounded-full' />
+                </small>
+                <small className='h-10 w-10 rounded-full bg-slate-200 flex items-center justify-center'>
+                    <Image src="images/facebk.svg" alt='facebook' width={30} height={30} className='rounded-full' />
+                </small>
             </div>
             <div className='border-t-[1px] border-t-gray-600 space-y-3 py-4 mt-6' >
                 < FooterService />
