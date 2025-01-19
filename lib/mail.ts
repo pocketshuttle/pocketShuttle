fb12c71fa142e0dffe15081e76239a2948c68b8c
Abusantos <abusomwansantos@gmail.com>
2025-01-19 15:42:13 +0100

    updated the verified email

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,13 +1,14 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const domain = "http://localhost:3000/";
+const domain = "https://app.pocketshuttle.com/";
+// const domain = "http://localhost:3000/";
 
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "onboarding@pocketshuttle.com",
     to: email,
     subject: "Thank you for joining PocketShuttle, please confirm your email",
     html: `
@@ -25,7 +26,7 @@ export const sendResetPasswordEmail = async (email: string, token: string) => {
   const passwordLink = `${domain}/reset-password?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "onboarding@pocketshuttle.com",
     to: email,
     subject: "Please click on the link to reset your password",
     html: `
