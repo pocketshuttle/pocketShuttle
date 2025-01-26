33b1ee2f0d1eabf602d7dacc45baba59c8827921
Abusantos <abusomwansantos@gmail.com>
2025-01-26 10:13:31 +0100

    updated the prefetch

--- app/(auth)/style/footer/footer.tsx
+++ app/(auth)/style/footer/footer.tsx
@@ -8,10 +8,10 @@ const poppins = Poppins({ weight: "300", subsets: ["latin"] });
 
 export const AuthFooter = () => {
     return (
-        <div className={`${poppins.className} flex flex-col items-center px-3 py-5 space-y-4`}>
-            <div className={`border-t-[1px] border-t-gray-600 space-y-3 py-4 mt-6`}>
+        <div className={`${poppins.className} flex flex-col items-center px-3 md:py-5 `}>
+            <div className={`border-t-[1px] border-t-gray-600 gap-2 md:mt-6 flex flex-col items-center`}>
                 <FooterService />
-                <small className={` text-slate-500 py-4`}>Copyright @ 2024 PocketShuttle, all rights reserved</small>
+                <small className={` text-slate-500 py-2 text-center`}>Copyright @ 2024 PocketShuttle, all rights reserved</small>
             </div>
         </div>
     );
