dc24c3c8b2247f464a72aecc41a9e21664454781
Abusantos <abusomwansantos@gmail.com>
2025-08-14 03:22:02 +0100

    added push singal

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
 import LoginButton from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+import OneSignal from 'react-onesignal';
 
 export default function Home() {
+  const testNotification = async () => {
+    try {
+      await OneSignal.Notifications.requestPermission();
+      console.log('Notification permission requested');
+    } catch (error) {
+      console.error('Error requesting notification:', error);
+    }
+  };
+  useEffect(() => {
+    testNotification();
+  }, []);
   return (
     <main className="flex min-h-screen h-full flex-col items-center justify-center text-gray-100 ">
       <div className="space-y-6 flex flex-col items-center justify-center">
