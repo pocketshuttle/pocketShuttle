aefee283b79a6533d72e9949d691a965da7d7c4b
Abusantos <abusomwansantos@gmail.com>
2025-08-23 23:48:20 +0100

    added onesignal to the folder

--- onesignal/onesignal-client.tsx
+++ onesignal/onesignal-client.tsx
@@ -14,6 +14,8 @@ export default function OneSignalClient({ children }: { children: React.ReactNod
                     appId: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID || '94c38e89-e166-4578-b796-5f7dcdac4b16',
                     safari_web_id: process.env.NEXT_PUBLIC_ONESIGNAL_SAFARI_WEB_ID || "web.onesignal.auto.09714a24-a3bb-414f-8109-d75a4f07e6fa",
                     allowLocalhostAsSecureOrigin: true,
+                    serviceWorkerParam: { scope: "/push/onesignal/" },
+                    serviceWorkerPath: "/push/onesignal/OneSignalSDKWorker.js",
                     //@ts-ignore
                     notifyButton: { enable: true }
                 });
