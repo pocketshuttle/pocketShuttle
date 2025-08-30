35f2f208c5b7cbb3d0cff41d802831083b6a448a
Abusantos <abusomwansantos@gmail.com>
2025-08-30 03:09:12 +0100

    added

--- actions/reset.ts
+++ actions/reset.ts
@@ -26,8 +26,7 @@ export const reset = async (values: z.infer<typeof ResetPasswordSchema>) => {
   }
 
   const resetPasswordToken = await generatePasswordResetToken(
-    //@ts-ignore
-    existingUser.email
+    existingUser.email || ""
   );
 
   await sendResetPasswordEmail(
