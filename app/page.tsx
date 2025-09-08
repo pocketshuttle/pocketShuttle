99af9bba29539906d4a0806e2b3ae46c58abcc03
Abusantos <abusomwansantos@gmail.com>
2025-09-08 11:10:00 +0100

    updaetd the welcome screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import LoginButton from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import { useEffect } from "react";
 import OneSignal from 'react-onesignal';
 
@@ -17,14 +18,26 @@ export default function Home() {
     testNotification();
   }, []);
   return (
-    <main className="flex min-h-screen h-full flex-col items-center justify-center text-gray-100 ">
-      <div className="space-y-6 flex flex-col items-center justify-center">
-        <h1 className="text-5xl font-semibold drop-shadow-md text-center "> pocketshuttle 📍 </h1>
-        <p className="text-lg "> checkin and checkout, a simple service</p>
+    <main className=" min-h-screen bg-white space-y-6">
+      <div className="bg-[#1d146d] h-[65vh] flex items-center justify-center m-auto rounded-bl-3xl rounded-br-3xl">
+        <Image
+          src="/images/buus.png"
+          width={350}
+          height={200}
+          alt="pocketshuttle logo"
 
-        <LoginButton>
-          <Button size={"lg"} >Login</Button>
-        </LoginButton>
+        />
+      </div>
+      <div className=" flex flex-col items-center justify-center space-y-3 h-3/6 text-blue-950">
+        <h1 className="text-3xl px-4 py-3 font-semibold ">Be in the journey. anytime, anywhere </h1>
+        <div className="px-3 space-y-6">
+          <p className=" mb-3"> Monitor your kids picked up and dropped off anywhere</p>
+
+          <LoginButton>
+            <Button size={"lg"} className=" w-full mt-2 bg-[#1d146d]" >Walk In</Button>
+          </LoginButton>
+
+        </div>
       </div>
     </main>
   );
