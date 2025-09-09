e3c6245e585a814143a767d7f25d091d9c7c8b0a
Abusantos <abusomwansantos@gmail.com>
2025-09-09 14:15:03 +0100

    edited the upload image

---
+++ actions/reports/get-bus-arrival.ts
@@ -0,0 +1,24 @@
+import { StudentProps } from "@/types";
+import { logBusArrival } from "./log-bus-arrival";
+import haversine from "haversine-distance";
+
+export async function checkBusArrival(
+  busCoords: [number, number],
+  studentCoords: [number, number], 
+  student: StudentProps,
+  teacherId: string
+): Promise<boolean> {
+  if (!busCoords || !studentCoords) return false;
+
+  const distance = haversine(
+    { lat: busCoords[0], lng: busCoords[1] },
+    { lat: studentCoords[0], lng: studentCoords[1] }
+  );
+
+  if (distance <= 100) {
+    await logBusArrival(student.id, teacherId, student?.parentId);
+    return true;
+  }
+
+  return false;
+}
