e956c61fbf034edc75df9d7cbd39be4aca77090a
Abusantos <abusomwansantos@gmail.com>
2025-09-15 08:28:46 +0100

    added

--- app/(auth)/style/footer/footer.tsx
+++ app/(auth)/style/footer/footer.tsx
@@ -11,7 +11,7 @@ export const AuthFooter = () => {
         <div className={`${poppins.className} flex flex-col items-center px-3 md:py-5 `}>
             <div className={`border-t-[1px] border-t-gray-600 gap-2 md:mt-6 flex flex-col items-center`}>
                 <FooterService />
-                <small className={` text-slate-500 py-2 text-center`}>Copyright @ 2024 PocketShuttle, all rights reserved</small>
+                <small className={` text-slate-500 py-2 text-center`}>Copyright @ 2025 PocketShuttle, all rights reserved</small>
             </div>
         </div>
     );
--- components/dashboard/header/header.tsx
+++ components/dashboard/header/header.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Image from "next/image"
 import { useState } from "react";
+import whitefullname from "@/public/Whitefullname.png"
 
 
 export const DashboardHeader = () => {
@@ -9,7 +10,7 @@ export const DashboardHeader = () => {
     return (
         <div className="flex items-center justify-start space-x-3 mb-3 cursor-pointer"
         >
-            <Image src="/whitefullname.png" alt="pocket shuttle" width={180} height={80} className="px-2 py-0" />
+            <Image src={whitefullname} alt="pocket shuttle" width={180} height={80} className="px-2 py-0" />
             {/* <p className="text-xl px-8">PocketShuttle</p> */}
         </div>
     )
