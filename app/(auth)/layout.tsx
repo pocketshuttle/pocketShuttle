da94da45ced1698beba62dc252c65b9b0d6bea59
Abusantos <abusomwansantos@gmail.com>
2025-09-15 08:28:46 +0100

    added

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image"
 import { AuthFooter } from "./style/footer/footer"
 import { LgScreen } from "./style/lg-screen"
+import whitefullname from "@/public/Whitefullname.png"
+
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="flex flex-col lg:flex-row lg:px-10  h-screen items-center justify-center lg:w-4/5">
             <div className='p-6 lg:hidden'>
-                < Image src="/whitefullname.png" alt="Pocket shuttle" width={400} height={200} />
+                < Image src={whitefullname} alt="Pocket shuttle" width={400} height={200} />
             </div>
             <div className="hidden lg:flex md:flex w-full items-center justify-center">
                 <LgScreen />
--- app/(auth)/style/lg-screen.tsx
+++ app/(auth)/style/lg-screen.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image"
 import { AuthFooter } from "./footer/footer"
+import whitefullname from "@/public/Whitefullname.png"
 
 export const LgScreen = () => {
     return (
         <div className="flex items-center justify-center flex-col" >
             <div className="flex flex-col items-center justify-center space-y-4">
-                < Image src="/whitefullname.png" alt="Pocket shuttle" width={400} height={200} />
+                < Image src={"/Whitefullname.png"} alt="Pocket shuttle" width={400} height={200} />
                 < Image src="/images/buus.png" alt="Pocket shuttle" width={400} height={200} />
             </div>
             <div className="space-y-3 mt-8 w-full ">
