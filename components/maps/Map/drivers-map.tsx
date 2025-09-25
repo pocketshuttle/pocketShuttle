3276570c70039afdba3b62970d1172cf3bcca3f9
Abusantos <abusomwansantos@gmail.com>
2025-09-25 23:55:28 +0100

    we deleted the doubke setting of school location

--- components/maps/Map/drivers-map.tsx
+++ components/maps/Map/drivers-map.tsx
@@ -10,6 +10,7 @@ import GoogleMapView from './google-map';
 import { useTeacherLocation } from '@/hooks/useTeacher-location';
 import { useSchoolTeacherLocations } from '@/hooks/useTeacherAblyLocation';
 import { ChannelProvider, useChannel } from 'ably/react';
+import { useSchoolTeachersLocations } from '@/hooks/useTeachersSocketLocation';
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX!;
 
@@ -21,17 +22,18 @@ type TeacherLocation = {
     longitude: number;
 };
 
-export const DriversLocation = () => {
-    const mapRef = useRef<any>(null);
+export const DriversLocation = ({ userId }) => {
     const [teachersLocations, setTeachersLocations] = useState<TeacherLocation[]>([]);
     const [mySchoolLocation, setSchoolLocation] = useState({ latitude: 0, longitude: 0 });
     const [schoolLocation, setschoolLocation] = useState({ latitude: 0, longitude: 0 });
     const [isMapLoaded, setIsMapLoaded] = useState(false);
 
-    const teachers = useSchoolTeacherLocations()
+    // const teachers = useSchoolTeacherLocations()
+    const teachersLocation = useSchoolTeachersLocations(userId)
 
-    console.log(teachers)
-    console.log(mySchoolLocation)
+    useEffect(() => {
+        console.log("Updated teachers:", teachersLocation);
+    }, [teachersLocation]);
 
     useEffect(() => {
 
@@ -39,12 +41,9 @@ export const DriversLocation = () => {
             if (navigator.geolocation) {
                 const pos = navigator.geolocation.getCurrentPosition(pos => {
                     const { latitude, longitude } = pos.coords;
-                    setschoolLocation({ latitude, longitude });
-                    setSchoolLocation({ latitude, longitude });
-                    // console.log("School location updated:", { latitude, longitude });
+                    // Also set mySchoolLocation
+                    setSchoolLocation({ latitude, longitude }); 
                 });
-
-                console.log("Geolocation watchPosition started:", pos);
 
             } else {
                 console.log("Geolocation is not supported by this browser.");
@@ -56,23 +55,16 @@ export const DriversLocation = () => {
 
     return (
         <div className="w-full h-full">
-
-            <GoogleMapView latitude={mySchoolLocation.latitude} longitude={mySchoolLocation.longitude} teachersLocation={teachers} />
-
-            {!isMapLoaded && (
-                <div className="flex items-center justify-center h-[700px]">
-                    <p>Loading map...</p>
-                </div>
-            )}
+            <GoogleMapView latitude={mySchoolLocation.latitude} longitude={mySchoolLocation.longitude} teachersLocation={teachersLocation} />
         </div>
     );
 };
 
 
-export default function TeachersLocation() {
+export default function TeachersLocation({ userId }) {
     return (
         <ChannelProvider channelName="live-school-channel">
-            <DriversLocation />
+            <DriversLocation userId={userId} />
         </ChannelProvider>
     );
 }
