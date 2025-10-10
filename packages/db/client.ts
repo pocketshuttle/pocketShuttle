ffd76505d41141ab2928bb4a19d400b22d5d3004
Abusantos <abusomwansantos@gmail.com>
2025-10-10 09:41:26 +0100

    updated the rooy package.json

--- packages/db/client.ts
+++ packages/db/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "./node_modules/.prisma/client";
+import { PrismaClient } from ".prisma/client";
 export type { Prisma } from ".prisma/client";
 export type { StudentAttendance } from ".prisma/client";
 export type { StudentStatus } from ".prisma/client";
