26e5b79452e855298c55cd0c911be61bbbaf1788
Abusantos <abusomwansantos@gmail.com>
2026-06-22 13:13:35 +0100

    updated the ui aand made several  ui updates

--- packages/db/client.ts
+++ packages/db/client.ts
@@ -4,7 +4,7 @@ export type { StudentAttendance } from ".prisma/client";
 export type { StudentStatus } from ".prisma/client";
 export type { StudentPresence } from ".prisma/client";
 
-const PRISMA_SCHEMA_VERSION = "20260618110000_add_driver_last_active_at";
+const PRISMA_SCHEMA_VERSION = "20260622124500_add_support_ticket_deleted_at";
 
 const globalForPrisma = global as unknown as {
   prisma?: PrismaClient;
